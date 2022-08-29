3e03ce6ddd89f11322b03f7912ca76b19cd48017
Júlia <julia_l_de-oliveira@estudante.sc.senai.br>
2022-08-29 14:00:17 -0300

    generic controller

---
+++ api_pessoa/Controllers/GenericController.cs
@@ -0,0 +1,55 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_pessoa.Controllers
+{
+    public class GenericController<T> : ControllerBase where T : BaseModel
+    {
+        private BaseRepository<T> repo;
+
+        public GenericController()
+        {
+            this.repo = new BaseRepository<T>();
+        }
+
+        [HttpGet("{id}")]
+        public T GetById(int id)
+        {
+
+            return repo.GetById(id);
+
+        }
+
+        [HttpGet]
+        public List<T>GetAll()
+        {
+
+            return repo.GetAll();
+
+        }
+
+        [HttpPost]
+        public string Post(T model)
+        {
+  
+            return repo.Create(model);
+        }
+        [HttpDelete]
+        public string Delete(int id)
+        {
+  
+            return repo.Delete(id);
+
+        }
+        [HttpPut]
+        public string Update(T model)
+        {
+
+
+            return repo.Update(model);
+
+        }
+
+    }
+}
